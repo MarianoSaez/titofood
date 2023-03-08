c159487999cebdbaf6bb42e43bb4aea094cd7277
Bruno <brunobonil@gmail.com>
2023-03-07 21:19:25 -0300

    VentaResource terminado

--- src/main/java/ar/edu/um/fi/programacion2/domain/Venta.java
+++ src/main/java/ar/edu/um/fi/programacion2/domain/Venta.java
@@ -35,7 +35,7 @@ public class Venta implements Serializable {
     @Column(name = "foreign_id")
     private Double foreignId;
 
-    @OneToMany(mappedBy = "venta")
+    @OneToMany(mappedBy = "venta", fetch = FetchType.EAGER)
     @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @JsonIgnoreProperties(value = { "venta", "menu" }, allowSetters = true)
     private Set<DetalleVenta> detalleVentas = new HashSet<>();
